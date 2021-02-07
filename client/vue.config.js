a524b032b4203808beb8a62ae5faf28e919ea718
Bret Papkoff <bretpapkoff@gmail.com>
2021-02-07 04:36:29 -0500

    AutoLint On Save Fix

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   publicPath: '',
+  chainWebpack: config => {
+    config.module
+      .rule('eslint')
+      .use('eslint-loader')
+      .options({
+        fix: true,
+      });
+  },
   pluginOptions: {
     cordovaPath: 'src-cordova'
   }
