68ba1622097265a043609133e5d8b63853704dcf
Bret Papkoff <bretpapkoff@gmail.com>
2021-02-14 00:50:06 -0500

    Allow Production Client With Cors Policy

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ const cors = require("cors");
 // })
 
 const corsOptions = {
-  origin: "http://localhost:8080",
+  origin: ["http://localhost:8080","http://localhost:5000"],
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
